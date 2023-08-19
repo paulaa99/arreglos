76c9c544b4e7f9b3aabbb56d424f76621f4b24be
paulaa99 <140978441+paulaa99@users.noreply.github.com>
2023-08-19 17:35:15 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,43 @@
+// Array extraño
+const strangeArray = [
+  "Zero",
+  function () {
+    alert("Hola soy una función en un array");
+  },
+  22,
+  null,
+  "Go lang",
+  undefined,
+  "Cobol",
+  "I'm programmer",
+  -2000,
+  "Hello world",
+  `One is ${1}`,
+  { name: "Info", lastname: "last info" },
+  () => true,
+  function showNumbers() {
+    return "1, 2, 3, 4";
+  },
+  "Another String",
+  ["Hola mundo!"],
+  "b is a letter",
+  "JavaScript",
+];
+
+function showList(array) {
+  const container = document.getElementById("list");
+  container.innerHTML = "";
+  array.forEach((element) => {
+    const li = document.createElement("li");
+    li.appendChild(document.createTextNode(element));
+    container.appendChild(li);
+  });
+}
+
+document.addEventListener("DOMContentLoaded", (e) => {
+  const strings = strangeArray.filter((element) => typeof element === "string");
+
+  strings.sort((a, b) => a.localeCompare(b, undefined, { sensitivity: 'base' }));
+
+  showList(strings);
+});
